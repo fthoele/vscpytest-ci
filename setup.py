1bfb17a0c8c2b4cd517cf1613d1014341f734ed8
Florian Thoele <florian.thoele@mat.ethz.ch>
2018-05-26 22:25:35 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mypackage',
+      version='0.1',
+      description='test package',
+      url='https://github.com/fthoele/vscpytest-ci',
+      author='FT',
+      packages=['mypackage'],
+)
