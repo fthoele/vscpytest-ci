c53805ebb08e946e2d3ea53e4b27b463069527ff
Florian Thoele <florian.thoele@mat.ethz.ch>
2018-05-26 22:38:42 +0200

    changed package finding in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='mypackage',
       version='0.1',
       description='test package',
       url='https://github.com/fthoele/vscpytest-ci',
       author='FT',
-      packages=['mypackage'],
+      packages=find_packages(),
 )
