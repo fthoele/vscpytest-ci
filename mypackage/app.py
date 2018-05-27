1e1527bf19b978a8183e4263d2c012c2533328df
Florian Thoele <florian.thoele@mat.ethz.ch>
2018-05-27 08:02:25 +0200

    data dir in tests

--- mypackage/app.py
+++ mypackage/app.py
@@ -4,6 +4,10 @@ import numpy as np
 def foo(x):
   return np.mean(x)
 
+def foo_from_file(filename):
+  x = np.fromfile(filename, sep=" ")
+  return np.mean(x)
+    
 
 if __name__ == "__main__":
   x = foo([1,2])
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,27 @@
+from pytest import fixture
+
 from mypackage import app
+
+@fixture
+def datadir(request):
+    import os
+    filename = request.module.__file__
+    test_dir = os.path.dirname(filename)
+
+    def getter(filename):
+        return os.path.join(test_dir, "data", filename)
+
+    return getter
 
 def test_foo():
     s = app.foo([1,2])
     #assert s == "print foo"
     assert s == 1.5
+
+
+def test_foo_from_file(datadir):
+    filename = datadir("test.txt")
+    #assert filename == "test.txt"
+    
+    x = app.foo_from_file(filename)
+    assert x == 2.0
